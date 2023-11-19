85c5f43da05effb2c5f8a44eb00274be9b6fe881
Taufik Khan <taufik4051@gmail.com>
2023-11-19 18:12:35 +0530

    day3

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print(recursionStar(10, 10));
-    }
+        int[][] image = {{1,0,0}, {0,1,0}, {0,0,1}};
+        System.out.println(Arrays.deepToString(image));
+        for(int[] row : image){
+            for (int i = 0; i < (image[0].length + 1)/2; i++) {
+                int temp = row[i]^1;
+                row[i] = row[image[0].length - i - 1]^1;
+                row[image[0].length - i - 1] = temp;
+            }
+        }
+        System.out.println(Arrays.deepToString(image));
+        }
 
     static int add(int[] nums) {
         int start = 0;
